cffebb8070f876adda4ba4aea46ea787d6fe2fcd
beatface <emma.beaton@gmail.com>
2016-02-02 11:09:14 -0600

    refactored into express

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
-const http = require('http');
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
-http.createServer((req, res) => {
-  console.log(req.method, req.url);
+app.get('/hello', function (req, res) {
+  res.send('Hello World!');
+});
 
-  res.writeHead(PORT, {
-    "content-type": "text/html"
-  });
+app.get('/random', (req, res) => {
+	res.send(Math.random().toString());
+});
 
-  if(req.url === "/hello") {
-  	const msg = '<h1>Hello world!</h1>';
+app.get('*', function (req, res) {
+  res.send('OTHER!');
+});
 
-  	msg.split("").forEach((thing, i) => {
-  		setTimeout(() => {
-	  		res.write(thing)
-		}, 500 * i);
-  	});
-
-  	setTimeout(()=> {
-  		res.end('<h3>Bye world!</h3>');
-  	}, 10000);
-  } else if (req.url === "/random") {
-  	res.end(Math.random().toString());
-  } else {
-  	res.writeHead(403);
-  	res.end('Access denied');
-  }
-
-
-}).listen(PORT, () => {
-  console.log(`Node.js server started. Listening on port ${PORT}`);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
