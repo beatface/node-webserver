e67be11403f7c98942b52ab10679d473b117f021
beatface <emma.beaton@gmail.com>
2016-02-12 10:46:21 -0600

    adding mongoose changes

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const multer = require('multer');
 const imgur = require('imgur');
 const fs = require('fs');
 const crypto = require('crypto');
+const request = require('request');
+const _ = require('lodash');
+const cheerio = require('cheerio');
+const mongoose = require('mongoose');
+
+let db;
+
+// Connection URL
+const mongo_url = 'mongodb://localhost:27017/node-webserver';
+
+
 
 var storage = multer.diskStorage({
   destination: 'uploads/',
@@ -15,8 +26,6 @@ var storage = multer.diskStorage({
       if (err) { return cb(err) }
       cb(null, raw.toString('hex') + path.extname(file.originalname))
     })
-
-    // cb(null, Date.now() + path.extname(file.originalname))
   }
 })
 
@@ -30,9 +39,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.locals.title = 'super sick shit';
 
-// app.use(bodyParser.urlencoded({
-// 	extended: false
-// }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
@@ -52,9 +63,37 @@ app.get('/contact', function(req, res) {
 	res.render('contact');
 });
 
-app.post('/contact', (req, res) => {
-	res.render('contacted', { name: req.body.name });
+
+
+
+
+const contactSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    message: String
 });
+
+const Contact = mongoose.model('shit', contactSchema);
+
+app.post('/contact', (req, res) => {
+
+	console.log(">>>>>>>>>>>>>>>>>>>", req.body);
+	const obj = new Contact({
+		name: req.body.name,
+		email: req.body.email,
+		message: req.body.message
+	});
+
+	obj.save((err, obj) => {
+		if (err) throw err;
+		console.log(obj);
+		res.render('contacted', { name: req.body.name });
+	});
+});
+
+
+
+
 
 app.get('/send-photos', (req, res) => {
 	res.render('sendphoto');
@@ -78,15 +117,146 @@ app.post('/send-photos', upload.single('photo'), (req, res) => {
     });
 });
 
+// API route
+app.get('/api', (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.send({hello: "world"});
+});
+
+
+
+app.post('/api', (req, res) => {
+	console.log(req.body);
+	const obj = _.mapValues(req.body, val => val.toUpperCase());
+
+	db.collection('allcaps').insertOne(obj, (err, result) => {
+		if (err) throw err;
+		console.log(res);
+		res.send(result.ops[0]);
+	});
+
+});
+
+
+
+
+// API/WEATHER route
+app.get('/api/weather', (req, res) => {
+	const url = "https://api.forecast.io/forecast/46e68dca4c818903bec6865e4fa6e4c1/37.8267,-122.423";
+	request.get(url, (err, response, body) => {
+		if (err) throw err;
+		res.header('Access-Control-Allow-Origin', '*');
+		res.send(JSON.parse(body));
+	});
+});
+
+
+app.get('/api/topnews', (req, res) => {
+  db.collection('news').findOne({}, (err, doc) => {
+  	if (err) throw err;
+  	console.log(doc);
+  	res.render('topnews', { news: doc.top[0].title, url: doc.top[0].url });
+  });
+});
+
+
+
+
+
+const News = mongoose.model('news', mongoose.Schema({
+  top: [{title: String, url: String}]
+}));
+
+
+
+// webscraping
+app.get('/api/news', (req, res) => {
+  News.findOne().sort('-_id').exec((err, doc) => {
+    if (err) throw err;
+
+    if (doc) {
+      const FIFTEEN_MINUTES_IN_MS = 15 * 60 * 1000;
+      const diff = new Date() - doc._id.getTimestamp() - FIFTEEN_MINUTES_IN_MS;
+      const lessThan15MinutesAgo = diff < 0;
+
+      if (lessThan15MinutesAgo) {
+        res.send(doc);
+        return;
+      }
+    }
+
+    const url = 'http://cnn.com';
+
+    request.get(url, (err, response, html) => {
+      if (err) throw err;
+
+      const news = [];
+      const $ = cheerio.load(html);
+
+      const $bannerText = $('.banner-text');
+
+      news.push({
+        title: $bannerText.text(),
+        url: url + $bannerText.closest('a').attr('href')
+      });
+
+      const $cdHeadline = $('.cd__headline');
+
+      _.range(1, 12).forEach(i => {
+        const $headline = $cdHeadline.eq(i);
+
+        news.push({
+          title: $headline.text(),
+          url: url + $headline.find('a').attr('href')
+        });
+      });
+
+    const obj = new News({ top: news });
+
+    obj.save((err, result) => {
+        if (err) throw err;
+
+        res.send(news);
+     });
+
+      // db.collection('news').insertOne({ top: news }, (err, result) => {
+      //   if (err) throw err;
+
+      //   res.send(news);
+      // });
+    });
+  });
+});
+
+mongoose.connect(mongo_url);
+
+const datab = mongoose.connection;
+datab.on('open', function() {
+	console.log("mongoose open!");
+	// if(err) throw err;
+	app.listen(PORT, function () {
+	  console.log(`Example app listening on port ${PORT}!`);
+	});
+	// we're connected!
+});
 
 
 
 
 
 
+// // Use connect method to connect to the Server ----- THIS IS WITH MONGODB DRIVER!!!!! (not mongoose)
+// MongoClient.connect(mongo_url, function(err, database) {
+// 	if (err) throw err;
+// 	console.log("Connected correctly to server", database);
 
+// 	db = database;
 
-
+// 	app.listen(PORT, function () {
+// 	  console.log(`Example app listening on port ${PORT}!`);
+// 	});
+// 	// db.close();
+// });
 
 
 
@@ -164,6 +334,3 @@ app.post('/send-photos', upload.single('photo'), (req, res) => {
 //   	res.send('Not allowed, bruh.');
 // });
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!');
-});
