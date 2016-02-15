27b07d5b152d24eab2189ad2017694b3a0bbfd62
beatface <emma.beaton@gmail.com>
2016-02-15 13:19:47 -0600

    x

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 } else {
 	var mongo_url = 'mongodb://localhost:27017/node-webserver';
 }
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
